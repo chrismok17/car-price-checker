730493437b4c97630336336e8443a14f478a89d6
Chris M <chrismok97@gmail.com>
2022-06-29 20:50:51 -0700

    extracted vehicle information, need to add to dict or json with price

--- main.py
+++ main.py
@@ -1,47 +1,39 @@
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 def hyundai_surrey():
-    url = "https://www.jphyundaisurrey.com/new-inventory/index.htm?search=&model=Elantra&gvBodyStyle=Sedan"
-    # page = requests.get(url)
-
-    # soup = BeautifulSoup(page.content, "html.parser")
-    # result = soup.find_all("div", class_="vehicle-card-body ddc-font-size-small d-sm-flex flex-sm-row")
+    url = "https://www.jphyundaisurrey.com/showroom/index.htm"
     path = r'C:\\Users\\chrismok\Documents\\geckodriver.exe'
     driver = webdriver.Firefox(executable_path=path)
     driver.get(url)
-    title = driver.title
-    driver.implicitly_wait(0.5)
-    # driver.find_element("vehicle-card-body ddc-font-size-small d-sm-flex flex-sm-row").send_keys(".")
-
-    # js = "document.getElementByClass('vehicle-card-body ddc-font-size-small d-sm-flex flex-sm-row').options[1].text = '100';"
-    # driver.execute_script(js)
-    driver.quit()
-    return title
-
-def main():
-    print(hyundai_surrey())
-
-    # path = r'C:\\Users\\chrismok\Documents\\geckodriver.exe'
-    # driver = webdriver.Firefox(executable_path=path)
-    # driver.get("https://google.com")
 
     # title = driver.title
+    
+    car = driver.find_elements(By.CLASS_NAME, "fn  ")
+    price = driver.find_elements(By.CLASS_NAME, "price.h3")
 
-    # driver.implicitly_wait(0.5)
+    driver.implicitly_wait(5)
 
-    # search_box = driver.find_element(by=By.Name, value="q")
-    # search_button = driver.find_element(by=By.NAME, value="btnK")
+    with open("vehicles.json", "w") as jsonfile:
+        pass
 
-    # search_box.send_keys("Selenium")
-    # search_button.click()
+    for i in car:
+        vehicles = {
+            "vehicle": i.text.replace("\n", " "),
+        }
+        
+        
+        print(f'Printing: {vehicles}')
 
-    # value = search_box.get_attribute("value")
+    driver.quit()
+    
+    
+    # print(car)
+    # return span
 
-    # driver.quit()
+def main():
+    hyundai_surrey()
 
 if __name__ == "__main__":
     main()
